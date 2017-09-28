27acf06a6a57d26a9ca5acaa935d24a94a8914a4
Taehyeon Jake Lee <nayalth@gmail.com>
2017-09-28 15:38:52 +0900

    Add select process

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ContactInfo from './ContactInfo';
+import ContactDetails from './ContactDetails';
 
 export default class Contact extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
+            selectedKey: -1,
             keyword: '',
             contactData: [{
                 name: 'Abet',
@@ -23,12 +25,20 @@ export default class Contact extends React.Component {
         };
 
         this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
     }
 
     handleChange(e) {
         this.setState({
             keyword: e.target.value
         });
+    }
+
+    handleClick(key) {
+        this.setState({
+            selectedKey: key
+        });
+        console.log(key, 'is selected');
     }
 
     render() {
@@ -40,7 +50,10 @@ export default class Contact extends React.Component {
                 }
             );
             return data.map((contact, i) => {
-                return (<ContactInfo contact={contact} key={i}/>);
+                return (<ContactInfo
+                            contact={contact}
+                            key={i}
+                            onClick={() => this.handleClick(i)}/>);
             });
         };
 
@@ -54,6 +67,10 @@ export default class Contact extends React.Component {
                     onChange={this.handleChange}
                 />
                 <div>{mapToComponents(this.state.contactData)}</div>
+                <ContactDetails
+                    isSelected={this.state.selecetedKey != -1}
+                    contact={this.state.contactData[this.state.selectedKey]}
+                />
             </div>
         );
     }
